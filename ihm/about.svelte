21333b551a1773840e14a0046d0db7ac092fe190
clementuu <clementcalia@gmail.com>
2026-03-26 15:10:18 +0100

    mise en gras des mots clés pour la page à propos

--- ihm/about.svelte
+++ ihm/about.svelte
@@ -16,8 +16,8 @@
                 Mon premier contact avec l'informatique remonte à l'enfance, j'était le petit-fils que les grands-parents appelaient
                 à la rescousse quand l'ordinateur faisait des siennes, J'allais bidoullier un peu dans le panneau de navigation 
                 et c'était très souvent c'était suffisant.
-                <br>J'ai toujours été un vrai « touche à tout ». J'ai toujours eu besoin d'explorer, de comprendre, 
-                de tester. Cette curiosité m'a suivi partout : dans mes lectures, mes projets, mes passions… et plus tard, 
+                <br>J'ai toujours été un vrai « touche à tout ». J'ai toujours eu besoin d'<b>explorer</b>, de <b>comprendre</b>, 
+                de <b>tester</b>. Cette curiosité m'a suivi partout : dans mes lectures, mes projets, mes passions… et plus tard, 
                 dans ma manière d'aborder l'ingénierie.
             </p>
 
@@ -33,28 +33,29 @@
                 Après ma licence en mathématiques et informatique appliquées aux sciences humaines et sociales, un service 
                 civique m'a fait découvrir la gestion de projet et l'impact concret qu'un outil bien pensé peut avoir. 
                 C'est ce qui m'a poussé à poursuivre en master d'expert en ingénierie logicielle, avec l'envie de créer 
-                des applications utiles, accessibles et robustes.
+                des applications <b>utiles</b>, <b>accessibles</b> et <b>robustes</b>.
             </p>
             <p>
                 Depuis, j'ai travaillé sur des projets variés : création d'un jeu éducatif, 
-                automatisation de tests End-2-End d'un logiciel utilisant la reconnaissance faciale, refonte d'une application de caisse… 
-                Des expériences qui ont renforcé mon goût pour les solutions simples, maintenables et tournées vers l'humain.
+                <b>automatisation de tests</b> End-2-End d'un logiciel utilisant la reconnaissance faciale, <b>refonte d'une application</b> de caisse, 
+                <b>modernisation d'un SI</b> pour des épiceries solidaires… 
+                <br>Des expériences qui ont renforcé mon goût pour les solutions simples, maintenables et tournées vers l'humain.
             </p>
 
             <h4>Ce que j'apporte</h4>
             <p>
-                La curiosité, la rigueur et le sens du partage sont au cœur de ma manière de travailler. J'aime comprendre 
+                La <b>curiosité</b>, la <b>rigueur</b> et le <b>sens du partage</b> sont au cœur de ma manière de travailler. J'aime comprendre 
                 en profondeur, documenter ce que j'apprends, structurer ce que je fais et faciliter la vie de ceux qui 
                 passeront derrière moi. 
                 Douze années de sports collectifs - rugby puis handball - m'ont appris l'importance du collectif, de 
-                l'écoute et de l'humilité. Ces réflexes se retrouvent aujourd'hui dans ma façon de collaborer, d'animer 
+                <b>l'écoute</b> et de <b>l'humilité</b>. Ces réflexes se retrouvent aujourd'hui dans ma façon de collaborer, d'animer 
                 des réunions ou de préparer des présentations professionnelles.
             </p>
 
             <h4>Ce qui me nourrit</h4>
             <p>
-                En dehors du code, je me ressource dans la musique : je joue de la guitare depuis le collège et je produis 
-                un peu quand j'ai le temps. Les sciences et la culture alimentent ma curiosité, et le sport m'aide à garder 
+                En dehors du code, je me ressource dans la <b>musique</b> : je joue de la guitare depuis le collège et je produis 
+                un peu quand j'ai le temps. Les <b>sciences</b> et la <b>culture</b> alimentent ma curiosité, et le <b>sport</b> m'aide à garder 
                 un bon équilibre. 
                 Ces centres d'intérêt ne sont pas des activités à côté : ils influencent ma manière de penser, de créer, 
                 d'aborder les problèmes. Ils m'aident à garder les idées fraîches et à relier des univers qui, au premier 
